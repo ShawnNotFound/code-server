4346ae6b5d8bcfb5805d1f2f3efc951ef504d9b9
ShawnNotFound <ssj13363381203@163.com>
2023-03-12 11:21:46 +0000

    new file:   Shawn/Luogu/test.cpp 	new file:   executable_files/test

---
+++ Shawn/Luogu/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if(a == 1)  cout << "good!" << endl;
+    else cout << "not bad!" << endl;
+
+    return 0;
+}
