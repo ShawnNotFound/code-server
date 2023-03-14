5c7889b0d974ada831e19357fcc54f520dada253
ShawnNotFound <ssj13363381203@163.com>
2023-03-14 12:18:16 +0000

    modified:   "Shawn/Luogu/P4822 [BJWC2012]\345\206\273\347\273\223.cpp"

--- Shawn/Luogu/P4822 [BJWC2012]冻结.cpp
+++ Shawn/Luogu/P4822 [BJWC2012]冻结.cpp
@@ -2,23 +2,67 @@
 #include<cmath>
 #include<cstring>
 #include<algorithm>
+#include<queue>
 
 const int N = 50 + 5;
 const int M = 1e3 + 5; 
+const int K = 50 + 5;
 
 using namespace std;
 
-int map[N][N];
+int head[N * K], ver[M * K], edge[M * K], nxt[M * K] tot;
+bool v[M * K];
+int n, m, k, a, b, t, ans = 0x3f;
+
+priority_queue<pair<int, int>> q;
+int d[M * K];
+
+void add(int x, int y, int z)
+{
+    ver[++ tot] = y;
+    edge[tot] = z;
+    nxt[tot] = head[x];
+    head[x] = tot;
+}
+
+void build(int x, int y, int z)
+{
+    for(int i = 0; i <= k - 1; i ++)
+    {
+        add(n * i + x, n * (i + 1) + y, z / 2);
+    }
+}
+
+void dijkstra()
+{
+    memset(d, 0x3f, sizeof(d));
+    memset(v, false, sizeof(v));
+    d[1] = 0;
+    q.push(make_pair(0, 1));
+
+    while(q.size())
+    {
+        int x = q.top().second; q.pop();
+        
+    }
+}
 
 int main()
 {
-    int n, m, k, a, b, t;
     cin >> n >> m >> k;
     for(int i = 1; i <= n; i ++)
     {
         cin >> a >> b >> t;
-        map[a][b] = t;
+        build(a, b, t);
+        build(b, a, t);
     }
+
+    dijkstra();
+
+    for(int i = 1; i <= k; i ++)
+        ans = min(ans, d[n * i]);
+
+    cout << ans;
     
     return 0;
 }
