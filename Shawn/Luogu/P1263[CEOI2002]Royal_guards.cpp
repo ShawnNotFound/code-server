133b299342bc73347fdfa5811d1ecfcb5ed99146
ShawnNotFound <ssj13363381203@163.com>
2023-03-19 11:05:02 +0000

    new file:   Shawn/Luogu/P1263[CEOI2002]Royal_guards.cpp 	modified:   "Shawn/Luogu/P2756\351\243\236\350\241\214\345\221\230\351\205\215\345\257\271\346\226\271\346\241\210\351\227\256\351\242\230.cpp" 	new file:   "Shawn/Luogu/P2825[HEOI2016TJOI2016]\346\270\270\346\210\217.cpp" 	new file:   Shawn/Luogu/core 	new file:   executable_files/P1263[CEOI2002]Royal_guards 	modified:   "executable_files/P2756\351\243\236\350\241\214\345\221\230\351\205\215\345\257\271\346\226\271\346\241\210\351\227\256\351\242\230" 	new file:   "executable_files/P2825[HEOI2016TJOI2016]\346\270\270\346\210\217" 	new file:   "executable_files/TJOI2016]\346\270\270\346\210\217"

---
+++ Shawn/Luogu/P1263[CEOI2002]Royal_guards.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+
+const int N = 2e2 + 10;
+const int INF = 1 << 29;
+
+using namespace std;
+
+int map[N][N];
+int m, n, ltot, rtot, tot = 1, s, t, cnt;
+bool flag = true;
+int head[N], ver[N], edge[N], nxt[N], d[N], now[N], raw_edge[N], rst[N];
+int maxflow, u, v;
+queue<int> q;
+
+struct tai_nan_le_wo_yao_tui_sai
+{
+    int num, x, y;
+}l[N], r[N];// 用于二分图匹配
+
+void add(int x, int y, int z)
+{
+    ver[++ tot] = y, edge[tot] = z, nxt[tot] = head[x], head[x] = tot;
+    ver[++ tot] = x, edge[tot] = 0, nxt[tot] = head[y], head[y] = tot;
+}
+
+bool bfs()
+{
+    memset(d, 0, sizeof(d));
+    while(q.size())
+        q.pop();
+    
+    q.push(s); d[s] = 1;
+    now[s] = head[s];
+
+    while(q.size())
+    {
+        int x = q.front(); q.pop();
+        for(int i = head[x]; i; i = nxt[i])
+        {
+            int y = ver[i];
+            if(!d[y] && edge[i])
+            {
+                d[y] = d[x] + 1;
+                q.push(y);
+                now[y] = head[y];
+                if(y == t)
+                    return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int dinic(int x, int flow)
+{
+    if(x == t)  
+        return flow;
+    int rest = flow, k, i;
+    for(i = now[x]; i && rest; i = nxt[i])
+    {
+        int y = ver[i];
+        now[x] = i;
+        if(edge[i] && d[y] == d[x] + 1)
+        {
+            k = dinic(y, min(rest, edge[i]));
+            if(!k)
+                d[y] = INF;
+            edge[i] -= k;
+            edge[i ^ 1] += k;
+            rest -= k;
+        }
+    }
+    return flow - rest;
+}
+
+int main()
+{
+    // for(int i = 0; i <= 10; i ++)
+    //     for(int j = 0; j <= 10; j ++)
+    //         map[i][j] = 3;
+
+    
+
+    cin >> m >> n;
+    for(int i = 1; i <= m; i ++)
+        for(int j = 1; j <= n; j ++)
+            cin >> map[i][j];
+
+    // for(int i = 0; i <= 5; i ++)
+    // {
+    //     for(int j = 0; j <= 5; j ++)
+    //         cout << map[i][j] << " ";
+    //     cout << endl;
+    // }
+
+    for(int i = 1; i <= m; i ++)
+    {
+        for(int j = 1; j <= n; j ++)
+        {
+            if(flag && map[i][j] == 0)  
+            {
+                l[++ ltot] = {++ cnt, i, j};
+                flag = false;
+            }
+            if(map[i][j] == 2)
+                flag = true;
+        }
+        flag = true;
+    }
+        
+    for(int i = 1; i <= n; i ++)
+    {
+        flag = true;
+        for(int j = 1; j <= m; j ++)
+        {
+            if(flag && map[i][j] == 0)  
+            {
+                r[++ rtot] = {++ cnt, i, j};
+                flag = false;
+            }
+            if(map[i][j] == 2)
+                flag = true;
+        }
+    }
+
+    s = cnt + 2, t = cnt + 3;
+
+    for(int i = 1; i <= ltot; i ++)
+        add(s, i, 1);
+    for(int i = ltot + 1; i <= rtot + ltot; i ++)
+        add(i, t, 1);
+    for(int i = 1; i <= ltot; i ++)
+        for(int j = ltot + 1; j <= rtot + ltot; j ++)
+        {
+            add(i, j, INF);
+        }
+
+    int flow = 0;
+    while(bfs())
+        while(flow = dinic(s, INF)) 
+            maxflow += flow;
+
+    cout << maxflow << endl;
+
+    for(int i = 2;i <= ltot;i += 2)
+    {
+        if(ver[i] != s && ver[i ^ 1] != s)
+        if(ver[i] != t && ver[i ^ 1] != t)
+        if(edge[i ^ 1] != 0)
+        {
+            //cout << map[l[ver[i ^ 1]].x][l[ver[i ^ 1]].y] << endl;
+            cout << l[ver[i ^ 1]].x << " " << l[ver[i ^ 1]].y << endl;
+        }
+    }
+    for(int i = ltot + 2;i <= tot;i += 2)
+    {
+        if(ver[i] != s && ver[i ^ 1] != s)
+        if(ver[i] != t && ver[i ^ 1] != t)
+        if(edge[i ^ 1] != 0)
+        {
+            //cout << map[r[ver[i ^ 1]].x][r[ver[i ^ 1]].y] << endl;
+            cout << r[ver[i ^ 1]].x << " " << r[ver[i ^ 1]].y << endl;
+        }
+    }
+
+    return 0;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+}
--- Shawn/Luogu/P2756飞行员配对方案问题.cpp
+++ Shawn/Luogu/P2756飞行员配对方案问题.cpp
@@ -10,14 +10,14 @@ const int INF = 1 << 29;
 using namespace std;
 
 bool flag[110][110];
-int head[N], ver[N], edge[N], nxt[N], d[N], now[N];
+int head[N], ver[N], edge[N], nxt[N], d[N], now[N], raw_edge[N], rst[N];
 int n, m, e, s, t, maxflow, u, v, tot = 1;
 queue<int> q;
 
 void add(int x, int y, int z)
 {
-    ver[++ tot] = y, edge[tot] = z, nxt[tot] = head[x], head[x] = tot;
-    ver[++ tot] = x, edge[tot] = 0, nxt[tot] = head[y], head[y] = tot;
+    ver[++ tot] = y, edge[tot] = z, raw_edge[tot] = z, nxt[tot] = head[x], head[x] = tot;
+    ver[++ tot] = x, edge[tot] = 0, raw_edge[tot] = z, nxt[tot] = head[y], head[y] = tot;
 }
 
 bool bfs()
@@ -65,6 +65,7 @@ int dinic(int x, int flow)
             edge[i] -= k;
             edge[i ^ 1] += k;
             rest -= k;
+            rst[i] = flow - rest;
         }
     }
     return flow - rest;
@@ -72,16 +73,16 @@ int dinic(int x, int flow)
 
 int main()
 {
-    cin >> n >> m;
-    s = n + m + 1, t = n + m + 2;
+    cin >> m >> n;
+    s = n + 1, t = n + 2;
     for(e = 1; 1; e ++)
     {
         cin >> u >> v;
         if(u == v && u == -1)
             break;
-        v += n;
+        //v += m;
         if(!flag[s][u])
-            add(s, u, 1);    
+            add(s, u, 1);
         flag[s][u] = true;
         //cout << s << " " << u << " " << 1 << endl;
 
@@ -90,7 +91,7 @@ int main()
         flag[v][t] = true;
         //cout << v << " " << t << " " << 1 << endl;
         if(!flag[u][v])
-            add(u, v, INF);
+            add(u, v, 1);
         flag[u][v] = true;
         //cout << u << " " << v << " " << 1 << endl;
     }
@@ -100,8 +101,34 @@ int main()
     while(bfs())
         while(flow = dinic(s, INF))
             maxflow += flow;
-
+            
     cout << maxflow << endl;
+
+
+
+    // for(int x = 1; x <= m; x ++)
+    // {
+    //     for(int i = head[x]; i; i = nxt[i])
+    //     {
+    //         int y = ver[i], flow = edge[i];
+    //         if(rst[i ^ 1] && x <= n && y <= n)
+    //             cout << x << " " << y << endl;
+    //         // if(flow >= raw_edge[i] || x > n || y > n) 
+    //         //     continue;
+    //         // else cout << x << " "  << y << endl; 
+    //         //cout << raw_edge[i ^ 1] << " " << flow << " " <<  << endl;
+    //     }
+    // }
+    
+    for(int i = 2;i <= tot;i += 2)
+    {
+        if(ver[i] != s && ver[i ^ 1] != s)
+        if(ver[i] != t && ver[i ^ 1] != t)
+        if(edge[i ^ 1] != 0)
+        {
+            cout << ver[i ^ 1] << " " << ver[i] << endl;
+        }
+    }
+
     return 0; 
 }
-
